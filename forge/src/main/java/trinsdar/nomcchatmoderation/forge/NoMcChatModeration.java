1b5794abb13987c41d3dea02dabb6529a31d2a74
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-06-25 20:54:54 -0400

    removed unnessecary classes and renamed stuff

---
+++ forge/src/main/java/trinsdar/nomcchatmoderation/forge/NoMcChatModeration.java
@@ -0,0 +1,8 @@
+package trinsdar.nomcchatmoderation.forge;
+
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(NoMcChatModeration.MODID)
+public class NoMcChatModeration {
+    public static final String MODID = "nomcchatmoderation";
+}
